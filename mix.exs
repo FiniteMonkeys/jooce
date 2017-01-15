912d0b1c11f896df17a408802a679f412b48b27f
Craig S. Cottingham <craig@cottingham.net>
2017-01-15 01:41:29 -0600

    Add dependency on connection

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Jooce.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :connection],
      mod: {Jooce.Application, []}]
   end
 
@@ -29,6 +29,6 @@ defmodule Jooce.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:connection, "~> 1.0.4"}]
   end
 end
