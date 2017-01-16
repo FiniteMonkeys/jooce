39816165cc07d48176b027e0d70b08908ae9ecc0
Craig S. Cottingham <craig@cottingham.net>
2017-01-15 22:42:59 -0600

    Change client test

--- test/jooce/client_test.exs
+++ test/jooce/client_test.exs
@@ -21,13 +21,8 @@ defmodule JooceClientTest do
   test "procedural Elixir" do
     IO.puts("connecting")
     {:ok, conn} = Jooce.Client.start_link
-    IO.puts("sending hello")
-    :ok = Jooce.Client.send(conn, <<0x48, 0x45, 0x4C, 0x4C, 0x4F, 0x2D, 0x52, 0x50, 0x43, 0x00, 0x00, 0x00>>)
-    IO.puts("sending name")
-    <<packet::binary-size(32), _::binary>> = "Jooce" <> String.duplicate(<<0>>, 32)
-    :ok = Jooce.Client.send(conn, packet)
-    IO.puts("receiving GUID")
-    {:ok, guid} = Jooce.Client.recv(conn, 16, :infinity)
+    IO.puts("getting guid")
+    {:ok, guid} = Jooce.Client.guid(conn)
     IO.puts(inspect guid)
     IO.puts("closing connection")
     :ok = Jooce.Client.close(conn)
