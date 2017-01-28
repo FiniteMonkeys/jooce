77086b0a153fc31002c5368575f552a6c4ff8512
Craig S. Cottingham <craig@cottingham.net>
2017-01-27 23:14:20 -0600

    Clean up lib/jooce/utils.ex

--- lib/jooce/utils.ex
+++ lib/jooce/utils.ex
@@ -6,16 +6,12 @@ defmodule Jooce.Utils do
   @doc """
   Reads a varint from the server.
 
-  ## Examples
-
-      iex> Jooce.Utils.read_varint(conn)
-
   """
   def read_varint(conn, buffer \\ <<>>) do
     case Jooce.Connection.recv(conn, 1) do
       {:ok, <<1 :: size(1), _ :: bitstring>> = byte} ->
         read_varint(conn, buffer <> byte)
-      {:ok, <<0::size(1), _::size(7)>> = byte} ->
+      {:ok, <<0 :: size(1), _ :: size(7)>> = byte} ->
         buffer <> byte
       _ ->
         buffer
