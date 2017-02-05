7931695371dfb71d878b1ebfde5401eb167a724e
Craig S. Cottingham <craig@cottingham.net>
2017-02-05 16:44:06 -0600

    Add GravityTurn handler

--- examples/orbital.exs
+++ examples/orbital.exs
@@ -2,21 +2,70 @@ defmodule Orbital do
   defmodule StageOnMECO do
     use GenEvent
 
-    def handle_event({:amount, :liquid_fuel, amount}, state) when amount <= 0.1 do
-      IO.puts "MECO"
-      Jooce.Monitor.Resources.remove_handler(state.resources_pid, Orbital.StageOnMECO, state)
-      {:ok, state}
+    def init(state) do
+      {:ok, Map.put(state, :enabled, true)}
     end
 
-    def handle_event({:amount, :liquid_fuel, amount}, state) do
-      # IO.puts amount
-      {:ok, state}
+    def handle_event({:amount, :liquid_fuel, amount}, %{enabled: enabled} = state) when (amount <= 0.1) and enabled do
+      IO.puts "MECO"
+      Jooce.Controller.Control.throttle(state.control_pid, 0.0)
+
+      IO.puts "Launch stage separation"
+      Jooce.Controller.Control.stage(state.control_pid)
+
+      Jooce.Controller.Autopilot.off(state.autopilot_pid)
+      Jooce.Controller.Autopilot.sas_on(state.autopilot_pid)
+      Jooce.Controller.Autopilot.sas_mode(state.autopilot_pid, :prograde)
+
+      # Jooce.Monitor.Resources.remove_handler(state.resources_pid, Orbital.StageOnMECO, state)
+      {:ok, %{state | enabled: false}}
     end
 
     def handle_event(_, state) do
       {:ok, state}
     end
   end
+
+  defmodule GravityTurn do
+    use GenEvent
+
+    def init(state) do
+      state = Map.put(state, :enabled, true)
+      state = Map.put(state, :pitch, 90.0)
+      {:ok, state}
+    end
+
+    def handle_event({:altitude, :mean, altitude}, %{enabled: enabled, pitch: old_pitch} = state) when (altitude > 500) and (altitude < 90_000) and enabled do
+      pitch = 90.0 - (:math.pow(altitude / 90_000, 0.4) * 90.0)
+      pitch = cond do
+                pitch < 0.01 ->
+                  0.01
+                pitch > 89.9 ->
+                  89.9
+                true ->
+                  pitch
+              end
+      if abs(old_pitch - pitch) > 0.5 do
+        # IO.puts "#{altitude}: #{pitch}"
+        {:ok, %{state | pitch: pitch}}
+      else
+        {:ok, state}
+      end
+    end
+
+    def handle_event({:altitude, :mean, altitude}, %{enabled: enabled} = state) when (altitude > 90_000) and enabled do
+      # Jooce.Monitor.Flight.remove_handler(state.flight_pid, Orbital.GravityTurn, state)
+      {:ok, %{state | enabled: false}}
+    end
+
+    def handle_event(_, state) do
+      {:ok, state}
+    end
+  end
+
+  ##
+  ## main script body
+  ##
 
   def go do
     state = initialize("Orbital")
@@ -32,9 +81,11 @@ defmodule Orbital do
     {:ok, flight_pid} = Jooce.Monitor.Flight.start_link(conn, vessel_id)
     {:ok, resources_pid} = Jooce.Monitor.Resources.start(conn, vessel_id)
 
-    Jooce.Monitor.Resources.add_handler(resources_pid, Orbital.StageOnMECO, %{resources_pid: resources_pid})
+    state = %{conn: conn, vessel_id: vessel_id, autopilot_pid: autopilot_pid, control_pid: control_pid, flight_pid: flight_pid, resources_pid: resources_pid}
+    Jooce.Monitor.Flight.add_handler(flight_pid, Orbital.GravityTurn, state)
+    Jooce.Monitor.Resources.add_handler(resources_pid, Orbital.StageOnMECO, state)
 
-    %{conn: conn, vessel_id: vessel_id, autopilot_pid: autopilot_pid, control_pid: control_pid, flight_pid: flight_pid, resources_pid: resources_pid}
+    state
   end
 
   def preflight(state) do
@@ -66,37 +117,11 @@ defmodule Orbital do
     fuel = Jooce.Monitor.Resources.liquid_fuel(state.resources_pid)
     cond do
       fuel <= 0.1 ->
-        IO.puts "Launch stage separation"
-        Jooce.Controller.Control.throttle(state.control_pid, 0.0)
-        Jooce.Controller.Control.stage(state.control_pid)
-        Jooce.Controller.Autopilot.off(state.autopilot_pid)
-        Jooce.Controller.Autopilot.sas_on(state.autopilot_pid)
-        Jooce.Controller.Autopilot.sas_mode(state.autopilot_pid, :prograde)
         coast_to_apoapsis(state)
       true ->
         gravity_turn(state)
     end
   end
-
-  # def adjust_gravity_turn(state) do
-  #   send state.flight_pid, {:altitude, :mean, self()}
-  #   receive do
-  #     {:ok, altitude} ->
-  #       angle = 90.0 - (:math.pow(altitude / 90_000, 0.4) * 90.0)
-  # #       # angle = cond do
-  # #       #           angle < 0.01 ->
-  # #       #             0.01
-  # #       #           angle > 89.9 ->
-  # #       #             89.9
-  # #       #           true ->
-  # #       #             angle
-  # #       #         end
-  #       IO.puts "#{altitude}: #{angle}"
-  # #       send state.autopilot_pid, {:pitch, angle}
-  # #       Process.sleep 1000
-  #       gravity_turn(state)
-  #   end
-  # end
 
   def coast_to_apoapsis(state, altitude \\ 0) do
     new_altitude = Jooce.Monitor.Flight.mean_altitude(state.flight_pid)
@@ -107,8 +132,9 @@ defmodule Orbital do
         Jooce.Controller.Autopilot.sas_on(state.autopilot_pid)
         Process.sleep 100
         Jooce.Controller.Autopilot.sas_mode(state.autopilot_pid, :retrograde)
-        # Process.sleep 100
-        # Jooce.Controller.Autopilot.roll(state.autopilot_pid, 0.0)
+        Jooce.Controller.Autopilot.roll(state.autopilot_pid, 0.0)
+        Jooce.Controller.Autopilot.on(state.autopilot_pid)
+        Process.sleep 100
         descent_phase(state)
       true ->
         Jooce.Controller.Autopilot.sas_off(state.autopilot_pid)
@@ -138,7 +164,6 @@ defmodule Orbital do
         landing_phase(state)
       true ->
         IO.puts("Landed")
-        # Jooce.stop(state.conn)
     end
   end
 end
