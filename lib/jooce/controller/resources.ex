74dcb04fa9729496bc3ddd3099fcdab14b684cb4
Craig S. Cottingham <craig@cottingham.net>
2017-02-05 15:43:20 -0600

    Comment out flight and resources controllers for now

--- lib/jooce/controller/resources.ex
+++ lib/jooce/controller/resources.ex
@@ -1,41 +1,41 @@
 defmodule Jooce.Controller.Resources do
-  use GenServer
-
-  ##
-  ## API
-  ##
-
-  def start(conn, vessel_id, name \\ __MODULE__) do
-    GenServer.start(__MODULE__, %{conn: conn, vessel_id: vessel_id}, name: name)
-  end
-
-  def start_link(conn, vessel_id, name \\ __MODULE__) do
-    GenServer.start_link(__MODULE__, %{conn: conn, vessel_id: vessel_id}, name: name)
-  end
-
-  def liquid_fuel(pid) do
-    GenServer.call(pid, {:amount, :liquid_fuel})
-  end
-
-  ##
-  ## callbacks
-  ##
-
-  def init(%{conn: conn, vessel_id: vessel_id}) do
-    {:ok, resources_id, _} = Jooce.SpaceCenter.vessel_get_resources(conn, vessel_id)
-    {:ok, %{conn: conn, resources_id: resources_id}}
-  end
-
-  def handle_call({:amount, :liquid_fuel}, _from, state) do
-    {:ok, amount, _} = Jooce.SpaceCenter.resources_amount(state.conn, state.resources_id, "LiquidFuel")
-    {:reply, amount, state}
-  end
-
-  # def handle_cast do
+  # use GenServer
   #
-  # end
-
-  # def handle_info do
+  # ##
+  # ## API
+  # ##
   #
+  # def start(conn, vessel_id, name \\ __MODULE__) do
+  #   GenServer.start(__MODULE__, %{conn: conn, vessel_id: vessel_id}, name: name)
   # end
+  #
+  # def start_link(conn, vessel_id, name \\ __MODULE__) do
+  #   GenServer.start_link(__MODULE__, %{conn: conn, vessel_id: vessel_id}, name: name)
+  # end
+  #
+  # def liquid_fuel(pid) do
+  #   GenServer.call(pid, {:amount, :liquid_fuel})
+  # end
+  #
+  # ##
+  # ## callbacks
+  # ##
+  #
+  # def init(%{conn: conn, vessel_id: vessel_id}) do
+  #   {:ok, resources_id, _} = Jooce.SpaceCenter.vessel_get_resources(conn, vessel_id)
+  #   {:ok, %{conn: conn, resources_id: resources_id}}
+  # end
+  #
+  # def handle_call({:amount, :liquid_fuel}, _from, state) do
+  #   {:ok, amount, _} = Jooce.SpaceCenter.resources_amount(state.conn, state.resources_id, "LiquidFuel")
+  #   {:reply, amount, state}
+  # end
+  #
+  # # def handle_cast do
+  # #
+  # # end
+  #
+  # # def handle_info do
+  # #
+  # # end
 end
